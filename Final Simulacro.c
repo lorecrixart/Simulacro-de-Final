0a14eed428e49e0a3cbe1932f4ee143415e2774a
lorecrixart <111024692+lorecrixart@users.noreply.github.com>
2022-08-15 11:53:30 -0300

    Update Final Simulacro.c

--- Final Simulacro.c
+++ Final Simulacro.c
@@ -1,79 +1,97 @@
-/*Una tienda de antigüedades muy conocida por realizar subastas de sus productos, se vio obligada a cambiar la forma de realizar sus ventas
-debido a la realidad sanitaria.
-La tienda antes de abrir sus puertas al público, carga la totalidad de artículos disponibles en el lote
-que expondrá para la subasta y el precio base de cada uno.
-Al comenzar el ingreso del público, el mismo podrá recorrer el salón completo,
-anotar en una ficha personal cada uno de los códigos de artículos por los que desea subastar, y al retirarse, el cliente completará la oferta
-que desea realizar por cada uno de los artículos en la terminal disponible para registrar la operación.
-Otro control que debe tener la tienda es que el aforo habilitado es del 50%, es decir, solamente puede haber 10
-clientes al mismo tiempo dentro del local. Pudiendo concurrir como máximo hasta 50 clientes durante toda la
-jornada.
-Se pide desarrollar un programa para administrar el lote subastado, sabiendo que cada lote no tiene más de
-300 artículos diferentes.
-Al comenzar la jornada se solicitara al empleado cargar los datos de los artículos al lote con la siguiente
-información:
-• ID de artículo (valores enteros entre 1000 y 3000 no necesariamente consecutivos)
-• Nombre, hasta 40 caracteres
-• Descripción, hasta 255 caracteres. (Dato no obligatorio)
-• Precio Base.
-La carga del lote finaliza cuando se ingresa el código de artículo 9999.
-Luego de la carga, estará disponible un menú para operar con los datos. Es importante que al finalizar cada
-acción seleccionada del menú vuelva al inicio del mismo, hasta que el usuario elija la opción de cerrar
-subasta.
-El menú debe contener las siguientes opciones:
-1. Registrar ingreso del cliente: Al ingresar el cliente al salón se deben almacenar los siguientes datos:
-DNI, Nombre, Mail y Teléfono de contacto (todos los datos son obligatorios).
+/*An antique store well known for holding auctions of its products, was forced to change the way it conducts its sales
+due to health realities.
+The store, before opening its doors to the public, uploads the total number of items available in the lot it will display for auction and the base price of each item.
+the auction and the base price of each item.
+When the public enters the store, they will be able to go through the entire room,
+write down in a personal file each of the item codes for which he/she wishes to bid, and when leaving, the client will complete the bidding for each of the items he/she wishes to bid.
+the items on the terminal available to register the transaction.
+Another control that the store must have is that the authorized capacity is 50%, i.e., there can only be 10 customers inside the store at the same time.
+customers inside the store at the same time. A maximum of 50 customers can be present during the entire day.
+day.
+It is requested to develop a program to manage the auctioned lot, knowing that each lot has no more than 300 different items.
+300 different items.
+At the beginning of the day the employee will be asked to load the data of the items to the lot with the following.
+information:
+- Item ID (integer values between 1000 and 3000 not necessarily consecutive).
+- Name, up to 40 characters
+- Description, up to 255 characters (not mandatory data)
+- Base Price.
+The batch upload ends when the item code 9999 is entered.
+After loading, a menu will be available to operate with the data. It is important that at the end of each
+selected action in the menu returns to the beginning of the menu, until the user chooses the option to close the auction.
+auction.
+The menu must contain the following options:
+1. Register client entry: When the client enters the room, the following data must be stored:
+DNI, Name, Mail and Contact telephone number (all data are mandatory).
 
 
-Se debe controlar que el cliente no exista cargado y que la capacidad del salón no supere los 10
-clientes en el interior en simultáneo.
-2. Registrar oferta del cliente: Se solicitará el DNI del cliente, validando su
- existencia, y se procederá a cargar la oferta para cada artículo, ingresando el código y oferta.
-Es importante validar que el código sea existente y que la oferta sea mayor o igual al valor base.
-En esta operación, y por cada artículo, se deberá validar si la oferta es mayor
-a la existente o mayor a la oferta base si es que aún no hay oferta registrada, y si se cumple la condición, registrar en el artículo, el DNI del cliente y la oferta realizada.
-Este menú finaliza cuando el cliente indica que no tiene más artículos para cargar.
-En ese momento el cliente se retira del salón y libera un cupo del aforo permitido.
-3. Cerrar subasta: Para poder realizar esta opción se pedirá el ingreso de un token
-de seguridad o palabra clave (la cual será: LT&7412) y si el ingreso es correcto se mostrará
-el listado SOLAMENTE de cada uno de los artículos que han recibido una oferta, junto con los datos del cliente ganador.
-Solo se permitirán tres intentos de ingreso del token, si no se ingresa el correcto,
-no finaliza el programa, sino que se muestra de nuevo el menú principal.*/
+It is necessary to check that the client is not already loaded and that the capacity of the salon does not exceed 10
+customers inside the room at the same time.
+2. Register the client's offer: The client's ID will be requested, validating its existence, and the
+ The offer will be loaded for each item, entering the code and offer.
+It is important to validate that the code exists and that the offer is greater than or equal to the base value.
+In this operation, and for each item, you must validate whether the bid is higher than the existing bid or higher than the base value.
+to the existing one or higher than the base bid if there is no registered bid yet, and if the condition is met, record the customer's ID number and the bid made on the item.
+This menu ends when the customer indicates that he/she has no more items to load.
+At that moment the customer leaves the room and frees a quota of the allowed capacity.
+3. Close auction: In order to perform this option you will be asked to enter a security token or keyword (which will be used for the auction).
+token or keyword (which will be: LT&7412) and if the entry is correct it will show the list ONLY of each one of
+the listing ONLY of each of the items that have received a bid, along with the winning customer's details.
+Only three attempts to enter the token will be allowed, if the correct token is not entered,
+the program is not terminated, but the main menu is displayed again.*/
+
 
 #include<stdio.h>
 #include<stdlib.h>
 #define v 300
 
-struct producto
-{
+struct producto{
+ 
  int id;
  char nombre[40];
  char descripcion[255];
  float preciobase;
  int cant;
+ int auxID;
 
 };
 
-void ingresarProductos(struct producto productos[])
- {
+void productosRepetidos(struct producto productos[]){
+ 
+     for (int i=0;i < productos[i].cant;i++){
+      
+            while (productos[i].id == productos[i].auxID || productos[i].id <1000 || productos[i].id >3000 || productos[i].id != 9999){
+             
+                printf ("ERROR. ID invalido.\nIngrese otro valor:\n");
+                scanf ("%d",&productos[i].id);
+            }
+    }
+}
+
+void ingresarProductos(struct producto productos[]){
+ 
     printf("-INGRESE PRODUCTOS-\n");
     printf("Ingresar 9999 para salir\n\n");
 
     int cantidad=0;
 
-  for (int i=0;i<v; i++) {
+  for (int i=0;i<v; i++){
 
     printf("ID.");
 
-    do
-    {
+    do{
+     
         printf ("Por favor ingrese un numero real entre 1000 y 3000:\n");
         scanf("%d", &productos[i].id);
+
+        productos[i].auxID = productos[i].id;
+
+        productosRepetidos(productos);
 
     }while (productos[i].id < 1000 || productos[i].id > 3000 && productos[i].id != 9999);
 
     if (productos[i].id == 9999)
-    break;
+    return;
 
     printf("Nombre: \n");
     scanf("%s",&productos[i].nombre);
@@ -90,8 +108,8 @@ void ingresarProductos(struct producto productos[])
   }
 }
 
-int main (void)
-{
+int main (void){
+ 
     struct producto productos[v];
 
     short opcion;
@@ -99,28 +117,28 @@ int main (void)
 
     printf ("BIENVENIDO\n\n");
 
-    while (opcion!=1)
-    {
+    while (opcion!=1){
+     
         printf ("Ingrese 1 para cargar articulos\n");
         printf ("\n\n");
         scanf ("%hd",&opcion);
 
-        if (opcion!=1)
-        {
+        if (opcion!=1){
+         
             printf ("OPCION INVALIDA\n");
             printf ("Se necesita un minimo de 1 articulo cargado para comenzar subasta\n");
         }
 
-    switch (opcion)
-    {
+    switch (opcion){
+      
     case 1:
         ingresarProductos(productos);
     break;
     }
 
     }
-    for (i=0;i<productos[i].cant;i++)
-    {
+    for (i=0;i<productos[i].cant;i++){
+     
             printf ("-MENU DE PRODUCTOS-\n\n");
             printf ("\n\n");
             printf ("\nARTICULO DISPONIBLE\n");
@@ -133,4 +151,5 @@ int main (void)
 
     return 0;
 }
+
 
